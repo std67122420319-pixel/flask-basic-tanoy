916b36f71d70abe6b32f6824f1123d8f7a33c25c
std67122420319-pixel <std.67122420319@ubru.ac.th>
2025-12-19 10:57:44 +0700

    edit route root

--- SERVER.py
+++ SERVER.py
@@ -1,21 +1,24 @@
 from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/')
 def index():
     return """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Flask Basic</title>
+        </head>
+        <body>
+            <h1>Home Page</h1>
+            <hr>
+        </body>
+        </html>
 """
-@app.route('/user/<name>/<age>')
+@app.route('/user/<name>/<int:age>')
 def my_name(name, age):
   return f'<h1> My name is {name}.I\'m {age+1} years old.</h1>'
 
