8740aaead08e5de0c2826a38b409c631f0851574
std67122420319-pixel <std.67122420319@ubru.ac.th>
2025-12-19 10:31:57 +0700

    add update

--- SERVER.py
+++ SERVER.py
@@ -15,26 +15,26 @@ def index():
 </body>
 </html>
 """
-@app.route('/user/<name>/<int:age>')
-def my_mame(name, age):
-    return f'<h1>My name is {name}.I\'m {age+1} years old.</h1>'
-
-#@app.route('/calculator/addition/<int:a>/<int:b>')
-#def addition(a,b):
-  #return f'<h1>{a} + {b} = {a+b}<h1>'
+@app.route('/user/<name>/<age>')
+def my_name(name, age):
+  return f'<h1> My name is {name}.I\'m {age+1} years old.</h1>'
 
 @app.route('/calculator/addition/<int:a>/<int:b>')
 def addition(a,b):
-    return f'<h1>{a} - {b} = {a-b}</h1>'
+  return f'<h1>{a} + {b} = {a+b}<h1>'
 
-@app.route('/calculator/addition/<int:a>/<int:b>')
-def addition(a,b):
-    return f'<h1>{a} * {b} = {a*b}</h1>'
+@app.route('/calculator/subtraction/<int:a>/<int:b>')
+def subtraction(a,b):
+  return f'<h1>{a} - {b} = {a-b}<h1>'
 
-app.route('/calculator/addition/<int:a>/<int:b>')
-def addition(a,b):
-    return f'<h1>{a} / {b} = {a/b}<int:a>/</h1>'
+@app.route('/calculator/multiplication/<int:a>/<int:b>')
+def multiplication(a,b):
+  return f'<h1>{a} * {b} = {a*b}<h1>'
 
-@app.route('/calculator/power/<float:basee>/<floar:exponent>')
+@app.route('/calculator/division/<int:a>/<int:b>')
+def division(a,b):
+  return f'<h1>{a} / {b} = {a/b}<h1>'
+
+@app.route('/calculator/power/<float:base>/<float:exponent>')
 def power(base,exponent):
-    return f'<h1>{base} <sup> {base} </sup> = {base**exponent}</h1>'
+  return f'<h1>{base} <sup> {base} </sup> = {base**exponent}<h1>'
